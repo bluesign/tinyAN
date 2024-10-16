d59a4e2d6eb3b593b46f18d9b7fc874e9c60e3ad
Deniz Mert Edincik <deniz@edincik.com>
2024-10-16 19:18:36 +0200

    wip

--- server/evmDebug.go
+++ server/evmDebug.go
@@ -195,7 +195,8 @@ func (d *DebugAPI) traceBlockInner(
 		return nil, err
 	}
 	base, _ := flow.StringToAddress("d421a63faae318f9")
-	snap := d.api.storage.LedgerSnapshot(cadenceHeight - 1)
+	snap := d.api.storage.LedgerSnapshot(cadenceHeight)
+
 	led := NewViewOnlyLedger(snap)
 	emulator := emulator2.NewEmulator(led, base)
 
