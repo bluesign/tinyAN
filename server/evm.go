238c59757e2d41e5a79b2506c20c9fe7bb4e4975
Deniz Mert Edincik <deniz@edincik.com>
2024-10-17 22:10:49 +0200

    wip

--- server/evm.go
+++ server/evm.go
@@ -381,6 +381,9 @@ func (v *ViewOnlyLedger) SetValue(owner, key, value []byte) (err error) {
 		Owner: string(owner),
 		Key:   string(key),
 	}
+	if hex.EncodeToString(value) == "000000000000df59cf00000000000047d8000000010000000000000002" {
+		panic("d")
+	}
 	v.writes[reg] = value
 	return nil
 }
