9f48b07b3c725036196fc8d9ffb91364b2a08858
Deniz Mert Edincik <deniz@edincik.com>
2024-10-18 11:35:47 +0200

    wip

--- server/adapter.go
+++ server/adapter.go
@@ -50,7 +50,7 @@ func (e *EntropyProviderPerBlockProvider) AtBlockID(blockID flowgo.Identifier) e
 	if err != nil {
 		fmt.Println("error getting entropy seed")
 	}
-	var beacon []byte
+	var beacon [96]byte = [96]byte{}
 	if len(block.ParentVoterSigData) > 96 {
 		beacon = block.ParentVoterSigData[len(block.ParentVoterSigData)-96:]
 		fmt.Println("beacon", hex.EncodeToString(beacon))
