f3ea751a9f4a581f6bb3f953522942b273768a18
Deniz Mert Edincik <deniz@edincik.com>
2024-10-21 09:33:42 +0000

    wip

--- storage/utils.go
+++ storage/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/bloom"
 	"github.com/onflow/flow-go/model/flow"
 	gethCommon "github.com/onflow/go-ethereum/common"
 )
@@ -25,7 +26,7 @@ var defaultPebbleOptions = &pebble.Options{
 	MaxOpenFiles:  16384,
 
 	// Writes are stopped when the sum of the queued memtable sizes exceeds MemTableStopWritesThreshold*MemTableSize.
-	MemTableSize:                64 << 20 << 5,
+	MemTableSize:                64 << 20,
 	MemTableStopWritesThreshold: 4,
 
 	// The default is 1.
@@ -33,6 +34,31 @@ var defaultPebbleOptions = &pebble.Options{
 }
 
 func MustOpenPebbleDB(path string) *pebble.DB {
+	opts := defaultPebbleOptions
+	for i := 0; i < len(opts.Levels); i++ {
+		l := &opts.Levels[i]
+		// The default is 4KiB (uncompressed), which is too small
+		// for good performance (esp. on stripped storage).
+		l.BlockSize = 32 << 10       // 32 KB
+		l.IndexBlockSize = 256 << 10 // 256 KB
+
+		// 10 bits per key yields a filter with <1% false positive rate.
+		//
+		// The bloom filter is speedsup our SeekPrefixGE by skipping
+		// sstables that do not contain the prefix.
+		l.FilterPolicy = bloom.FilterPolicy(10)
+		l.FilterType = pebble.TableFilter
+
+		if i > 0 {
+			// L0 starts at 2MiB, each level is 2x the previous.
+			l.TargetFileSize = opts.Levels[i-1].TargetFileSize * 2
+		}
+		l.EnsureDefaults()
+	}
+
+	opts.FlushSplitBytes = opts.Levels[0].TargetFileSize
+	opts.EnsureDefaults()
+
 	db, err := pebble.Open(path, defaultPebbleOptions)
 	if err != nil {
 		panic(fmt.Errorf("error opening db: %w", err))
