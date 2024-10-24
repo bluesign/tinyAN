0e2e4a3d7121ffbb8d786890e48b67d99fbcbf17
Deniz Mert Edincik <deniz@edincik.com>
2024-10-24 10:14:45 +0200

    wip

--- server/repl/repl.go
+++ server/repl/repl.go
@@ -288,7 +288,7 @@ func (consoleREPL *ConsoleREPL) suggest(d prompt.Document) []prompt.Suggest {
 		}
 	}
 
-	words := strings.Split(wordBeforeCursor, ".")
+	//words := strings.Split(wordBeforeCursor, ".")
 
 	return prompt.FilterHasPrefix(suggests, wordBeforeCursor, false)
 }
