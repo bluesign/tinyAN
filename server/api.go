ec0f88056036ab582a1da30025b80f40ad94f778
Deniz Mert Edincik <deniz@edincik.com>
2024-10-29 01:40:36 +0100

    wip

--- server/api.go
+++ server/api.go
@@ -222,7 +222,7 @@ func (m APIServer) AccountSize(w http.ResponseWriter, r *http.Request) {
 			size += sizeOf(child)
 		}
 
-		fmt.Println(fmt.Sprintf("storable %T", storable), storable)
+		fmt.Println(fmt.Sprintf("storable %T", storable), storable, size)
 		mapStorable, ok := storable.(*atree.MapDataSlab)
 		if ok {
 			mapStorable.PopIterate(persistentSlabStorage, func(key atree.Storable, value atree.Storable) {
