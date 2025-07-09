c91c8350f5bc230cf49fd844df75cca974391fc1
Deniz Mert Edincik <deniz@edincik.com>
2025-07-09 13:11:33 +0200

    x

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,34 @@
 package main
 
-func main(){
-	checkpointDb := MustOpenPebbleDB(fmt.Sprintf("db/%s/checkpoint", "mainnet26"))
-	ledgerDb := MustOpenPebbleDB(fmt.Sprintf("db/%s/ledger", "mainnet26"))
+import (
+	"fmt"
+	"github.com/bluesign/tinyAN/storage"
+	"github.com/cockroachdb/pebble"
+	"github.com/onflow/flow-go/ledger"
+	"github.com/onflow/flow-go/ledger/common/convert"
+	"github.com/onflow/flow-go/model/flow"
+	"strings"
+)
 
+func main() {
+	checkpointDb := storage.MustOpenPebbleDB(fmt.Sprintf("db/%s/checkpoint", "mainnet26"))
+	ledgerDb := storage.MustOpenPebbleDB(fmt.Sprintf("db/%s/ledger", "mainnet26"))
 
+	options := &pebble.IterOptions{
+		LowerBound: []byte(""),
+		UpperBound: []byte("\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"),
+	}
+	var k []byte
 
+	iter, err := checkpointDb.NewIter(options)
+	if err != nil {
+		return
+	}
+	for iter.Next() {
+
+		fmt.Println(string(iter.Key()))
+		break
+	}
+	defer iter.Close()
 
 }
